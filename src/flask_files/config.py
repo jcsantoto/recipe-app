8956c1255c1760f5accd28c1cf964f0bddc87733
ExEngineer <johncs1337@gmail.com>
2023-06-28 14:59:15 -0400

    Protecting secrets

--- src/flask_files/config.py
+++ src/flask_files/config.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
+import os
 
 class Config:
-    SECRET_KEY = "i6!IgiclHbbC+Out@O$@cz4^@:Bz(GSM5Ts{>1@cHI=QY0{t'>+NR27{lY^|s,C"
-    URI = "mongodb+srv://recipeapp:94cQWKQqdXeLGrhV@recipeapp.wsu3zml.mongodb.net/?retryWrites=true&w=majority"
-    sendgrid_key = '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'
+    load_dotenv()
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    URI = os.environ.get('URI')
+    sendgrid_key = os.environ.get('SENDGRID_API_KEY')
