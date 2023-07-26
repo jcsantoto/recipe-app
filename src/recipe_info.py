6f9990b6d06e6bf0dc57cd7f6f840900a20915be
Abdulai <Abdulai.b0001@gmail.com>
2023-07-26 14:03:59 -0400

    price and macros added to recipe page

--- src/recipe_info.py
+++ src/recipe_info.py
@@ -5,6 +5,8 @@ import src.recipe_info_util as util
 APIKEY = "?apiKey=b9f570c04c8a44229ffd38618ddfabe2"
 
 SEARCH_URL = "https://api.spoonacular.com/recipes/{id}/information"
+SEARCH_URL2 = "https://api.spoonacular.com/recipes/{id}/priceBreakdownWidget.json"
+SEARCH_URL3 ="https://api.spoonacular.com/recipes/{id}/nutritionWidget.json"
 
 
 class Recipe:
@@ -20,9 +22,17 @@ class Recipe:
             url = SEARCH_URL.replace("{id}", str(self.id)) + APIKEY
             self.recipe_info = requests.get(url).json()
 
+            url2 = SEARCH_URL2.replace("{id}", str(self.id)) + APIKEY
+            self.priceBreakDown = requests.get(url2).json()
+
+            url3 = SEARCH_URL3.replace("{id}", str(self.id)) + APIKEY
+            self.macros = requests.get(url3).json()
+
         elif response:
             self.id = response['id']
             self.recipe_info = response
+            self.priceBreakDown = response
+            self.macros = response
 
     def get_all(self):
         """
@@ -44,6 +54,17 @@ class Recipe:
         :return: Returns recipe price
         """
         return self.recipe_info['pricePerServing'] / 100
+
+    def get_total_Cost(self) -> str:
+
+        rounded = round((self.priceBreakDown['totalCost'] /100) ,2)
+
+
+        return "$"+ str(rounded)
+
+    def get_Macros(self) -> str:
+
+        return self.macros['nutrients']
 
     def get_prep_time(self) -> str:
         """
